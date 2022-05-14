f69d23c07cb1e1c2f6decb3571b8b7e6d703badf
ashish jangra <techashish12@gmail.com>
2022-05-14 21:31:37 +0530

    Update UPI-Link.js

--- UPI-Link.js
+++ UPI-Link.js
@@ -1,6 +1,6 @@
-function getUPILink(vpa, name,)
+function getUPILink(vpa, name,r,t)
 {
   var upiLink;
-  upiLink= "upi://pay?pa=" + vpa + "&pn=" + name;
+  upiLink= "upi://pay?pa=" + vpa + "&pn=" + name+"&am=" +r+"&cu="+t;
   return upiLink;
 }
